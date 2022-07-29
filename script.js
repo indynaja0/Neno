67d069d8ec0b9ce5f6914712a2f31c23f688d24d
indynaja0 <110257025+indynaja0@users.noreply.github.com>
2022-07-30 03:39:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*var sound = new Howl({
+  src: ['https://kennethjensen.net/sound/neon-city-ambience.ogg'],
+  html5: true,
+  preload: true,
+  volume: 0.2,
+  sprite: {
+    ambience: [0, 23995, true]
+  }
+});
+
+var gogo = sound.play('ambience');
+sound.fade(0, 0.2, 6000, gogo);*/
